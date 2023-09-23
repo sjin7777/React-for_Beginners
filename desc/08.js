55bac5d5da002b9ffcf760f0c380ff5a31c5781a
sjin7777 <so-jin@naver.com>
2023-09-23 18:08:13 +0900

    commit

---
+++ desc/08.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/* react-for-beginners-2019 */
+
+
+
+
+
+///* 8.0 - Introduction */
+///* 8.1 - Requirements */
+///* 8.2 - Theory Requirements */
+///* 8.3 - Why React */
+{
+    //  >> 설치 확인
+    //      - 'node -v'
+    //      - 'npm -v'
+    //      - 'npx -v'
+    //      - 'git --version'
+}
---
+++ desc/09.js
@@ -0,0 +1,58 @@
+'use strict';
+
+/* react-for-beginners-2019 */
+
+
+
+
+
+///* 9.0 - Creating your first React App */
+/* 프로젝트 생성 및 실행 */
+{
+    //  1. cra를 이용해서 프로젝트 생성
+    //          => npx create-react-app react-for-beginners-2019
+    //  2. 생성된 프로젝트 안에 폴더 및 파일 수정
+    //      2-1. README.md 파일
+    //          - 내용 다 삭제하고 간단하게 소개 작성
+    //      2-2. package.json 파일
+    //          - "scripts" - "test", "eject" 삭제
+    //      2-2. yarn.lock 파일
+    //          - 해당 파일이 있다면 파일 삭제
+    //  3. 생성한 프로젝트 실행
+    //          => npm start
+} 
+
+
+
+
+///* 9.1 - Creating a Github Repository */
+/* 생성한 프로젝트를 github에 넣기 */
+{
+    //  1. 기존 repository를 다시 초기화하라는 메시지를 주거나 또는 repository를 초기화하라고 뜸
+    //          => git init
+    //  2. github에서 repository 생성
+    //      - repository name: react-for-beginners-2019
+    //  3. 생성된 repository 주소 복사하여 git 원격 연결
+    //          => git remote add orgin https://github.com/sjin7777/저장소명.git
+    //  4. 작동하는지 확인
+    //          => git add . 
+    //  5. git에 commit
+    //          => git commit -m "커밋내용"
+    //  6. git에 push
+    //          => git push origin main
+    //  7. github에서 확인
+
+} 
+
+
+
+
+
+///* 9.2 - How does React work? */
+/* 생성한 프로젝트안에 있는 기본 파일 */
+{
+    //  1. src 폴더 안에 있는 파일 중, App.js와 index.js를 제외하고 나머지 파일들 다 삭제
+
+    //  2. index.js 파일, App.js 파일에서 기본적으로 필요한 코드 제외하고 나머지 코드 다 삭제
+    
+}
